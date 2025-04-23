b99ef76b721501eff69d959c6880795ff17d5c16
Andriyan1 <ytsivandriyn@gmail.com>
2025-04-23 15:09:56 +0300

    debag server

--- saidbar-using/saidbar-using.js
+++ saidbar-using/saidbar-using.js
@@ -7,21 +7,22 @@ function checkUserPanelVisibility() {
     sidebarNav.style.paddingTop = '0';
   }
 }
-checkUserPanelVisibility();
+
 
 document.addEventListener('DOMContentLoaded', function() {
   const token = getCookie('userToken');
   console.log('Token з cookies:', token);
 
   const userPanel = document.querySelector('.user-panel');
-  
   console.log('userPanel знайдено:', userPanel !== null);
 
   if (token && userPanel) {
     userPanel.style.display = 'block';
+    checkUserPanelVisibility();
     console.log('userPanel показано');
   } else {
     console.log('Токен або userPanel відсутній');
+    checkUserPanelVisibility();
   }
 });
 
@@ -29,3 +30,8 @@ function getCookie(name) {
   const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
   return match ? match[2] : null;
 }
+
+// Викликаємо функцію для перевірки видимості після того, як DOM готовий
+document.addEventListener('DOMContentLoaded', function() {
+  checkUserPanelVisibility();
+});
