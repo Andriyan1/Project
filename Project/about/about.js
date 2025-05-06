47db7fae527c69209912349e7596d5c33e542059
Andriyan1 <42609561+Andriyan1@users.noreply.github.com>
2025-05-06 13:53:27 +0300

    Delete Project directory

--- Project/about/about.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById("sidebar-toggle");
-const sidebar = document.getElementById("sidebar");
-
-toggleBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("closed");
-});
